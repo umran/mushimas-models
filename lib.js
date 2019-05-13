d682c3cbf5d9fc42e42033ffb8a480324f5c710c
Umran Hussein <umran.hussein@gmail.com>
2019-05-12 23:12:41 -0700

    created a separate model for active definitions

--- lib.js
+++ lib.js
@@ -1,3 +1,4 @@
 exports.Document = require('./src/Document')
 exports.Definition = require('./src/Definition')
 exports.Bucket = require('./src/Bucket')
+exports.ActiveDefinition = require('./src/ActiveDefinition')
---
+++ src/ActiveDefinition.js
@@ -0,0 +1,164 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const states = [
+  'DISABLED',
+  'ENABLED',
+  'DELETED'
+]
+
+const ItemOptions = Schema({
+  type: {
+    type: String,
+    required: true
+  },
+  required: {
+    type: Boolean,
+    required: true
+  },
+  enabled: {
+    type: Boolean,
+    required: true
+  },
+  es_indexed: {
+    type: Boolean,
+    required: true
+  },
+  es_keyword: {
+    type: Boolean,
+    required: false
+  },
+  es_boost: {
+    type: Number,
+    required: false
+  },
+  es_analyzer: {
+    type: String,
+    required: false
+  },
+  es_search_analyzer: {
+    type: String,
+    required: false
+  },
+  es_search_quote_analyzer: {
+    type: String,
+    required: false
+  },
+  ref: {
+    type: String,
+    required: false
+  }
+}, { _id: false })
+
+const Options = Schema({
+  type: {
+    type: String,
+    required: true
+  },
+  required: {
+    type: Boolean,
+    required: true
+  },
+  enabled: {
+    type: Boolean,
+    required: false
+  },
+  es_indexed: {
+    type: Boolean,
+    required: false
+  },
+  es_keyword: {
+    type: Boolean,
+    required: false
+  },
+  es_boost: {
+    type: Number,
+    required: false
+  },
+  es_analyzer: {
+    type: String,
+    required: false
+  },
+  es_search_analyzer: {
+    type: String,
+    required: false
+  },
+  es_search_quote_analyzer: {
+    type: String,
+    required: false
+  },
+  ref: {
+    type: String,
+    required: false
+  },
+  item: {
+    type: ItemOptions,
+    required: false
+  }
+}, { _id: false })
+
+const Field = Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  options: {
+    type: Options,
+    required: true
+  }
+}, { _id: false })
+
+const InnerDefinition = Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  class: {
+    type: String,
+    required: true
+  },
+  fields: {
+    type: [Field],
+    required: true
+  }
+}, { _id: false })
+
+const ActiveDefinition = Schema({
+  '@state': {
+    type: String,
+    required: true,
+    enum: states
+  },
+  '@lastModified': {
+    type: Date,
+    required: true
+  },
+  '@lastCommitted': {
+    type: Date,
+    required: true
+  },
+  '@version': {
+    type: Number,
+    required: true
+  },
+  '@idempotencyKey': {
+    type: String,
+    required: true
+  },
+  '@initialHash': {
+    type: String,
+    required: true
+  },
+  '@bucketId': {
+    type: String,
+    required: true
+  },
+  '@definition': {
+    type: InnerDefinition,
+    required: true
+  }
+})
+
+const model = mongoose.model('ActiveDefinition', ActiveDefinition)
+
+module.exports = model
