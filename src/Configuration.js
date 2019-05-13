8c364fa1c9a1999a1110d7a1a59a6041d122758d
Umran Hussein <umran.hussein@gmail.com>
2019-05-13 04:10:03 -0700

    include idempotencyKey and initialHash in the Configuration model

--- src/Configuration.js
+++ src/Configuration.js
@@ -25,6 +25,14 @@ const Configuration = Schema({
     type: Number,
     required: true
   },
+  '@idempotencyKey': {
+    type: String,
+    required: true
+  },
+  '@initialHash': {
+    type: String,
+    required: true
+  },
   '@bucketId': {
     type: String,
     required: true
